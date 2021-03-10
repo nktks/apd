bb96a6dadd40aab24438c53df3509efa273b6b4f
takeshi.nakata <takeshi.nakata@mixi.co.jp>
2021-03-10 14:16:31 +0900

    support recursive map or list structure

--- marshaler_test.go
+++ marshaler_test.go
@@ -17,35 +17,118 @@ func TestMarshaler_Append(t *testing.T) {
 		})
 		t.Run("can unmarshal", func(t *testing.T) {
 			t.Run("can find keys", func(t *testing.T) {
-				input := `[{"created_at" : "2020-01-01 00:00:00", "updated_at" : "2020-01-01 00:00:01"}]`
+				input := `
+[
+  {
+    "created_at" : "2020-01-01 00:00:00",
+    "updated_at" : "2020-01-01 00:00:01"
+  }
+]`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
-				require.Equal(t, "[\n  {\n    \"created_at\": \"2020-01-01 00:00:00\",\n    \"duration\": \"1s\",\n    \"updated_at\": \"2020-01-01 00:00:01\"\n  }\n]", output)
+				require.Equal(t, `[
+  {
+    "created_at": "2020-01-01 00:00:00",
+    "duration": "1s",
+    "updated_at": "2020-01-01 00:00:01"
+  }
+]`, output)
+
+			})
+			t.Run("nested map", func(t *testing.T) {
+				input := `
+[
+  {
+    "hoge" : {
+      "created_at" : "2020-01-01 00:00:00",
+      "updated_at" : "2020-01-01 00:00:01"
+    }
+  }
+]`
+				output, err := Append(kf, &marshaler, input)
+				require.NoError(t, err)
+				require.Equal(t, `[
+  {
+    "hoge": {
+      "created_at": "2020-01-01 00:00:00",
+      "duration": "1s",
+      "updated_at": "2020-01-01 00:00:01"
+    }
+  }
+]`, output)
+			})
+			t.Run("nested list", func(t *testing.T) {
+				input := `
+[
+  {
+    "hoge" : [
+      {
+        "created_at" : "2020-01-01 00:00:00",
+        "updated_at" : "2020-01-01 00:00:01"
+      }
+   ]
+  }
+]`
+				output, err := Append(kf, &marshaler, input)
+				require.NoError(t, err)
+				require.Equal(t, `[
+  {
+    "hoge": [
+      {
+        "created_at": "2020-01-01 00:00:00",
+        "duration": "1s",
+        "updated_at": "2020-01-01 00:00:01"
+      }
+    ]
+  }
+]`, output)
 			})
 			t.Run("cant find from key", func(t *testing.T) {
-				input := `[{"hoge" : "2020-01-01 00:00:00", "updated_at" : "2020-01-01 00:00:01"}]`
+				input := `
+[
+  {
+    "hoge" : "2020-01-01 00:00:00",
+    "updated_at" : "2020-01-01 00:00:01"
+  }
+]`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
-				require.Equal(t, "[\n  {\n    \"hoge\": \"2020-01-01 00:00:00\",\n    \"updated_at\": \"2020-01-01 00:00:01\"\n  }\n]", output)
+				require.Equal(t, `[
+  {
+    "hoge": "2020-01-01 00:00:00",
+    "updated_at": "2020-01-01 00:00:01"
+  }
+]`, output)
 			})
 			t.Run("cant find to key", func(t *testing.T) {
-				input := `[{"created_at" : "2020-01-01 00:00:00", "hoge" : "2020-01-01 00:00:01"}]`
+				input := `
+[
+  {
+    "created_at" : "2020-01-01 00:00:00",
+    "hoge" : "2020-01-01 00:00:01"
+  }
+]`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
 				require.Equal(t, "[\n  {\n    \"created_at\": \"2020-01-01 00:00:00\",\n    \"hoge\": \"2020-01-01 00:00:01\"\n  }\n]", output)
 			})
 			t.Run("not supported format", func(t *testing.T) {
 				t.Run("cant parse time", func(t *testing.T) {
-					input := `[{"created_at" : "hoge", "updated_at" : "2020-01-01 00:00:01"}]`
+					input := `
+[
+  {
+    "created_at" : "hoge",
+    "updated_at" : "2020-01-01 00:00:01"
+  }
+]`
 					output, err := Append(kf, &marshaler, input)
 					require.NoError(t, err)
-					require.Equal(t, "[\n  {\n    \"created_at\": \"hoge\",\n    \"updated_at\": \"2020-01-01 00:00:01\"\n  }\n]", output)
-				})
-				t.Run("does not list of flat maps", func(t *testing.T) {
-					input := `[{"hoge" : {"created_at" : "2020-01-01 00:00:00", "updated_at" : "2020-01-01 00:00:01"}}]`
-					output, err := Append(kf, &marshaler, input)
-					require.NoError(t, err)
-					require.Equal(t, "[\n  {\n    \"hoge\": {\n      \"created_at\": \"2020-01-01 00:00:00\",\n      \"updated_at\": \"2020-01-01 00:00:01\"\n    }\n  }\n]", output)
+					require.Equal(t, `[
+  {
+    "created_at": "hoge",
+    "updated_at": "2020-01-01 00:00:01"
+  }
+]`, output)
 				})
 			})
 		})
@@ -59,35 +142,78 @@ func TestMarshaler_Append(t *testing.T) {
 		})
 		t.Run("can unmarshal", func(t *testing.T) {
 			t.Run("can find keys", func(t *testing.T) {
-				input := "-  \"created_at\":  \"2020-01-01 00:00:00\"\n   \"updated_at\":  \"2020-01-01 00:00:01\"\n"
+				input := `
+-  "created_at":  "2020-01-01 00:00:00"
+   "updated_at":  "2020-01-01 00:00:01"
+`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
-				require.Equal(t, "- created_at: \"2020-01-01 00:00:00\"\n  duration: 1s\n  updated_at: \"2020-01-01 00:00:01\"\n", output)
+				require.Equal(t, `- created_at: "2020-01-01 00:00:00"
+  duration: 1s
+  updated_at: "2020-01-01 00:00:01"
+`, output)
+			})
+			t.Run("nested map", func(t *testing.T) {
+				input := `
+- hoge:
+    "created_at":  "2020-01-01 00:00:00"
+    "updated_at":  "2020-01-01 00:00:01"
+`
+				output, err := Append(kf, &marshaler, input)
+				require.NoError(t, err)
+				require.Equal(t, `- hoge:
+    created_at: "2020-01-01 00:00:00"
+    duration: 1s
+    updated_at: "2020-01-01 00:00:01"
+`, output)
+			})
+			t.Run("nested list", func(t *testing.T) {
+				input := `
+- hoge:
+  - "created_at":  "2020-01-01 00:00:00"
+    "updated_at":  "2020-01-01 00:00:01"
+`
+				output, err := Append(kf, &marshaler, input)
+				require.NoError(t, err)
+				require.Equal(t, `- hoge:
+  - created_at: "2020-01-01 00:00:00"
+    duration: 1s
+    updated_at: "2020-01-01 00:00:01"
+`, output)
 			})
 			t.Run("cant find from key", func(t *testing.T) {
-				input := "-  \"hoge\":  \"2020-01-01 00:00:00\"\n   \"updated_at\":  \"2020-01-01 00:00:01\"\n"
+				input := `
+-  "hoge":  "2020-01-01 00:00:00"
+   "updated_at":  "2020-01-01 00:00:01"
+`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
-				require.Equal(t, "- hoge: \"2020-01-01 00:00:00\"\n  updated_at: \"2020-01-01 00:00:01\"\n", output)
+				require.Equal(t, `- hoge: "2020-01-01 00:00:00"
+  updated_at: "2020-01-01 00:00:01"
+`, output)
 			})
 			t.Run("cant find to key", func(t *testing.T) {
-				input := "-  \"created_at\":  \"2020-01-01 00:00:00\"\n   \"hoge\":  \"2020-01-01 00:00:01\"\n"
+				input := `
+- "created_at":  "2020-01-01 00:00:00"
+  "hoge":  "2020-01-01 00:00:01"
+`
 				output, err := Append(kf, &marshaler, input)
 				require.NoError(t, err)
-				require.Equal(t, "- created_at: \"2020-01-01 00:00:00\"\n  hoge: \"2020-01-01 00:00:01\"\n", output)
+				require.Equal(t, `- created_at: "2020-01-01 00:00:00"
+  hoge: "2020-01-01 00:00:01"
+`, output)
 			})
 			t.Run("not supported format", func(t *testing.T) {
 				t.Run("cant parse time", func(t *testing.T) {
-					input := "-  \"created_at\":  \"hoge\"\n   \"updated_at\":  \"2020-01-01 00:00:01\"\n"
+					input := `
+-  "created_at":  "hoge"
+   "updated_at":  "2020-01-01 00:00:01"
+`
 					output, err := Append(kf, &marshaler, input)
 					require.NoError(t, err)
-					require.Equal(t, "- created_at: hoge\n  updated_at: \"2020-01-01 00:00:01\"\n", output)
-				})
-				t.Run("does not list of flat maps", func(t *testing.T) {
-					input := "-  hoge:\n   -  \"created_at\":  \"2020-01-01 00:00:00\"\n      \"updated_at\":  \"2020-01-01 00:00:01\"\n"
-					output, err := Append(kf, &marshaler, input)
-					require.NoError(t, err)
-					require.Equal(t, "- hoge:\n  - created_at: \"2020-01-01 00:00:00\"\n    updated_at: \"2020-01-01 00:00:01\"\n", output)
+					require.Equal(t, `- created_at: hoge
+  updated_at: "2020-01-01 00:00:01"
+`, output)
 				})
 			})
 		})
